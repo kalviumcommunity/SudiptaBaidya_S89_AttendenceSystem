a3f68fb49ca203a073832ecdab6df30c41bb5b44
SudiptaBaidya <sudiptab556677@gmail.com>
2025-08-11 11:38:24 +0530

    feat: Initial project setup (Part 1)

---
+++ AttendenceSystem/src/com/school/main.java
@@ -0,0 +1,8 @@
+package AttendenceSystem.src.com.school;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Attendance System");
+        System.out.println("Session 1: Project setup and Orientation complete.");
+    }
+}
